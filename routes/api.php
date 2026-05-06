a521b758533d3568a23947532e34cd49483731d5
Ubuntu <ubuntu@vps-d63f3e55.vps.ovh.net>
2026-05-06 14:17:43 +0000

    fix: add missing <?php tag in routes/api.php (was rendering as plain text)

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,10 @@
+<?php
 
 use App\Http\Controllers\Api\ItemController;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/items',                [ItemController::class, 'index']);
-    Route::get('/items/trending',       [ItemController::class, 'trending']);
-    Route::get('/items/craft',          [ItemController::class, 'craftOpportunities']);
-    Route::get('/items/{slug}',         [ItemController::class, 'show']);
+    Route::get('/items',          [ItemController::class, 'index']);
+    Route::get('/items/trending', [ItemController::class, 'trending']);
+    Route::get('/items/craft',    [ItemController::class, 'craftOpportunities']);
+    Route::get('/items/{slug}',   [ItemController::class, 'show']);
 });
